7f242bf58188fb343d3d256224aa58f48768b12c
Mateusz <kroplewskimateusz@gmail.com>
2023-06-28 13:29:31 +0100

    change mobile login page design

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,19 +1,17 @@
 
-
 export const LoginForm = ()=>{
 
     return(
-        <>
-            <form className="w-[250px] mx-auto mt-10">
-                <label htmlFor="email" className="block font-semibold text-primaryText/70">Email:</label>
-                <input type="email" name="email" id="email" placeholder="JohnDoe@gmail.com" className="mb-5 border-b-accent border-b-2 w-[200px] focus:outline-0 font-semibold text-primaryText"/>
+        <section className="relative z-50">
+            <form className="w-[250px] h-[300px] mx-auto mt-10  grid place-content-center rounded-sm">
+                <label htmlFor="email" className="block font-semibold text-primaryText/70 text-[25px]">Email:</label>
+                <input type="email" name="email" id="email" placeholder="JohnDoe@gmail.com" className="bg-accent/0 mb-5 border-b-accent border-b-2 w-[200px] focus:outline-0 font-semibold text-primaryText text-[20px] h-[40px]"/>
 
-                <label htmlFor="password" className="block font-semibold text-primaryText/70">Password:</label>
-                <input type="password" name="password" id="password" placeholder="Password" className="border-b-accent border-b-2 w-[200px] focus:outline-0 "/>
+                <label htmlFor="password" className="block font-semibold text-primaryText/70 text-[25px]">Password:</label>
+                <input type="password" name="password" id="password" placeholder="Password" className="bg-accent/0 font-semibold border-b-accent border-b-2 w-[200px] focus:outline-0 text-[20px] h-[40px]"/>
 
-                <button type="submit" className="block w-[150px] h-[35px] font-semibold border-primaryText border-dashed border-2 relative z-50 mt-10">Login</button>
+                <button type="submit" className="block w-[170px] h-[40px] font-semibold border-primaryText border-dashed border-2 relative z-50 mt-10 hover:font-bold hover:text-primaryText/80">Login</button>
             </form>
-
-        </>
+        </section>
     )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,7 +5,7 @@ import { LoginIcon } from "../components/svg/LoginIcon"
 
 export const Login = ()=>{
     return(
-        <section className="h-[100vh] w-[100vw]">
+        <section className="min-h-[100vh] w-[100vw] bg-gradient-to-t from-accent pb-10">
             <Link to="/">
                 <div className="hover:cursor-pointer flex w-[200px] mx-auto">
                     <div className="flex self-center ml-5">
@@ -21,10 +21,11 @@ export const Login = ()=>{
                 <p className="font-bold text-center text-primaryText/90 text-[35px] pt-5 z-10">Login</p>
             </div>
             <LoginForm />
-            <div className="absolute bottom-0  h-[300px] w-[300px] rotate-[10deg] overflow-hidden">
-                    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                        <path fill="#46DDB5" d="M40.4,-29.5C51.3,-18.5,58.5,-1.8,57.4,17.2C56.3,36.2,47.1,57.3,31.8,63.9C16.5,70.4,-4.8,62.3,-25.9,52.6C-47.1,42.9,-68.1,31.6,-70,17.5C-72,3.5,-54.9,-13.4,-40.1,-25.3C-25.3,-37.2,-12.6,-44.2,1.1,-45C14.7,-45.9,29.5,-40.6,40.4,-29.5Z" transform="translate(100 100)" />
-                    </svg>
+            <div className=" mt-16 w-[170px] mx-auto">
+                <p className="text-secondaryText/80 font-semibold text-[18px]">Don't have an account and want to register?</p>
+                <div className="w-[170px] h-[40px]">
+                    <button className="block w-[170px] h-[40px] font-semibold border-secondaryButton/80 border-dashed border-2 relative z-50 mt-10 hover:font-bold text-secondaryText/80">Register</button>
+                </div>
 
             </div>
         </section>
