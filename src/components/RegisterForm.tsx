67b4c39ad1ab91dec1b161cbcb5aa3b949cde8ab
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-06-30 15:12:42 +0100

    started form validation on register form

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,9 +1,24 @@
+import { SubmitHandler, useForm } from "react-hook-form"
 
+
+type Inputs = {
+    FirstName:string,
+    LastName:string,
+    Email:string,
+    Password:string,
+}
 
 export const RegisterForm = ()=>{
+
+    const {register,handleSubmit,formState: { errors },} = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        console.log(data)
+    }
+
     return(
         <section className="relative z-50">
-            <form className="w-[250px] md:w-[500px] mx-auto mt-10  grid place-content-center rounded-sm">
+            <form className="w-[250px] md:w-[500px] mx-auto mt-10  grid place-content-center rounded-sm" onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex md:flex-row flex-col mb-5">
                     <label htmlFor="ProfileImage" className="inline font-semibold text-primaryText/70 text-[25px]">Profile Image:</label>
                     <input type="file" name="ProfileImage" id="ProfileImage" className="md:ml-5 md:mt-[5px]" />
@@ -11,23 +26,19 @@ export const RegisterForm = ()=>{
                 <div className="md:flex md:flex-wrap">
                     <div className="md:mr-5">
                         <label htmlFor="FirstName" className="block font-semibold text-primaryText/70 text-[25px]">First Name:</label>
-                        <input type="text" name="FirstName" id="FirstName" placeholder="John" className="bg-accent/0 mb-5 border-b-accent border-b-2 w-[200px] focus:outline-0 font-semibold text-primaryText text-[20px] h-[40px]"/>
+                        <input type="text" id="FirstName" {...register("FirstName")} placeholder="John" className="bg-accent/0 mb-5 border-b-accent border-b-2 w-[200px] focus:outline-0 font-semibold text-primaryText text-[20px] h-[40px]"/>
                     </div>
                     <div>
                         <label htmlFor="LastName" className="block font-semibold text-primaryText/70 text-[25px]">Last Name:</label>
-                        <input type="text" name="LastName" id="LastName" placeholder="Doe" className="bg-accent/0 mb-5 border-b-accent border-b-2 w-[200px] focus:outline-0 font-semibold text-primaryText text-[20px] h-[40px]"/>
+                        <input type="text" {...register("LastName")} id="LastName" placeholder="Doe" className="bg-accent/0 mb-5 border-b-accent border-b-2 w-[200px] focus:outline-0 font-semibold text-primaryText text-[20px] h-[40px]"/>
                     </div>
                     <div className="md:mr-5">
                         <label htmlFor="email" className="block font-semibold text-primaryText/70 text-[25px]">Email:</label>
-                        <input type="email" name="email" id="email" placeholder="JohnDoe@gmail.com" className="bg-accent/0 mb-5 border-b-accent border-b-2 w-[200px] focus:outline-0 font-semibold text-primaryText text-[20px] h-[40px]"/>
+                        <input type="email" {...register("Email")} id="email" placeholder="JohnDoe@gmail.com" className="bg-accent/0 mb-5 border-b-accent border-b-2 w-[200px] focus:outline-0 font-semibold text-primaryText text-[20px] h-[40px]"/>
                     </div>
                     <div>
                         <label htmlFor="password" className="block font-semibold text-primaryText/70 text-[25px]">Password:</label>
-                        <input type="password" name="password" id="password" placeholder="Password" className="bg-accent/0 font-semibold border-b-accent border-b-2 w-[200px] focus:outline-0 text-[20px] h-[40px] mb-5"/>
-                    </div>
-                    <div>
-                        <label htmlFor="confirmPassword" className="block font-semibold text-primaryText/70 text-[25px]">Confirm Password:</label>
-                        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" className="bg-accent/0 font-semibold border-b-accent border-b-2 w-[200px] focus:outline-0 text-[20px] h-[40px]"/>
+                        <input type="password" {...register("Password")} id="password" placeholder="Password" className="bg-accent/0 font-semibold border-b-accent border-b-2 w-[200px] focus:outline-0 text-[20px] h-[40px] mb-5"/>
                     </div>
                 </div>
 
