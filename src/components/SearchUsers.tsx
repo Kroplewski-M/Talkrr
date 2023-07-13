bd2a58bcd8e375e8d4b667b6b97fc701f56a36b9
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-07-13 08:33:32 +0100

    creating chat documents when clicked on new user

--- src/components/SearchUsers.tsx
+++ src/components/SearchUsers.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { Search } from "./svg/Search";
-import { collection,getDocs,or,query,where } from "firebase/firestore";
+import { collection,doc,getDoc,getDocs,or,query,serverTimestamp,setDoc,updateDoc,where } from "firebase/firestore";
 import { db } from "../firebase";
 import { useUserInfo } from "../context/User";
 interface users{
@@ -66,6 +66,45 @@ export const SearchUsers = ({setIsSearching}:SearchUsersPros)=>{
             searchUsers();
         }
     }
+    const handleSelect = async(selectedId:string,selectedName:string,selectedPhotoUrl:string)=>{
+        //CHECK IF CHAT EXISTS IF NOT CREATE
+        const combinedID = userInfo.uid > selectedId? userInfo.uid + selectedId: selectedId + userInfo.uid;
+        try{
+            const chatRef = doc(db,"chats",combinedID); 
+            const res = await getDoc(chatRef);
+
+            if(!res.exists()){
+                //CREATE CHAT IN COLLECTION
+                await setDoc(doc(db,"chats",combinedID),{messages:[]});
+
+                //CREATE USER CHATS
+                await updateDoc(doc(db,"userChats",userInfo.uid),{
+                    [combinedID+".userInfo"]:{
+                        uid:selectedId,
+                        displayName:selectedName,
+                        photoUrl:selectedPhotoUrl,
+                    },
+                    [combinedID+".date"]:serverTimestamp()
+                })
+                await updateDoc(doc(db,"userChats",selectedId),{
+                    [combinedID+".userInfo"]:{
+                        uid:userInfo.uid,
+                        displayName:userInfo.displayName,
+                        photoUrl:userInfo.photoUrl,
+                    },
+                    [combinedID+".date"]:serverTimestamp()
+                })
+            }
+
+        }catch(error){
+            console.log(error);
+        }finally{
+            setIsSearching(false);
+            setNoResults(false);
+            setUsers([]);
+        }
+        
+    }
     return(
         <div className="w-[100%] h-[50px] flex relative">
             <div className="w-[80%] ml-5 pt-[15px]">
@@ -80,7 +119,7 @@ export const SearchUsers = ({setIsSearching}:SearchUsersPros)=>{
                         <p className="font-semibold pl-5 pt-5 mb-5">Results:</p>
                         {
                             users.map(user=>(
-                                <div className="w-[100%] h-[70px] bg-black/10 mb-[5px] flex z-50 hover:cursor-pointer hover:bg-black/40 rounded-sm" key={user.uid}>
+                                <div className="w-[100%] h-[70px] bg-black/10 mb-[5px] flex z-50 hover:cursor-pointer hover:bg-black/40 rounded-sm" key={user.uid} onClick={()=>handleSelect(user.uid,user.displayName,user.photoUrl)}>
                                     <img src={user.photoUrl} alt="" className="w-[40px] h-[40px] rounded-full flex self-center ml-5" />
                                     <p className="ml-5 flex self-center font-bold">{user.displayName}</p>
                                     <hr />
