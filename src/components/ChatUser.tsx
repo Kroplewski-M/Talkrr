2aba9c901dd15fda3b0d81702efffd59d574eef6
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-08-01 22:09:46 +0100

    can clear selected images

--- src/components/ChatUser.tsx
+++ src/components/ChatUser.tsx
@@ -8,6 +8,7 @@ import { UploadImg } from "./svg/UploadImg";
 import { v4 as uuidv4 } from 'uuid';
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { Message } from "./Message";
+import { Close } from "./svg/Close";
 
 interface ChatUserProps{
     back:()=>void,
@@ -19,6 +20,7 @@ export const ChatUser=({back}:ChatUserProps)=>{
     
     const [text,setText] = useState("");
     const [img,setImg] = useState<File | null>(null);
+    const [selectedImgURL,setSelectedImgURL] = useState('');
 
     useEffect(()=>{
         if(selectedMessage != '' && selectedMessage != undefined){
@@ -39,7 +41,6 @@ export const ChatUser=({back}:ChatUserProps)=>{
         if(e.target.files[0] !=undefined){
             setImg(e.target.files[0]);
         }
-        console.log(e.target.files[0]);
     }   
 
     const metadata = {
@@ -130,16 +131,29 @@ export const ChatUser=({back}:ChatUserProps)=>{
                         ))
                     }
                 </div>
-                <div className="absolute bottom-0 w-[100%] h-[100px] bg-accent/10 flex pt-5 " >
-                    <input type="text" className="w-[80%] h-[40px] pl-5 bg-black/0 focus:outline-none text-primaryText font-semibold" placeholder="Type something..." value={text} onKeyDown={handleKey} onChange={setMessageValue}/>
-                    <div className="mr-5 w-[40px] h-[35px]">
-                        <label htmlFor="img" className="hover:cursor-pointer">
-                            <UploadImg width={40} height={40} fill="#333333"/>
-                        </label>
-                            <input type="file" name="img" id="img" accept="image/png, image/jpeg" onChange={changeImg} className="w-[100%] h-[100%] bg-accent font-semibold hidden" />
+                <div className="absolute bottom-0 w-[100%] h-[100px] bg-accent/10 pt-5 " >
+                    <div className="flex absolute -mt-5 ml-5">
+                        <p className=" font-light text-primaryText">{img?.name}</p>
+                        {
+                            img != null?(<>
+                                <div className="hover:cursor-pointer" onClick={()=>setImg(null)}>
+                                    <Close width={30} height={30} fill="#FF0000" />
+                                </div>
+                            </>):(<></>)
+                        }
+
                     </div>
-                    <div className="mr-5 w-[100px] h-[35px]">
-                        <button className="w-[100%] h-[100%] bg-accent font-semibold" onClick={sendMessage}>Send</button>
+                    <div className="flex">
+                        <input type="text" className="w-[80%] h-[40px] pl-5 bg-black/0 focus:outline-none text-primaryText font-semibold" placeholder="Type something..." value={text} onKeyDown={handleKey} onChange={setMessageValue}/>
+                        <div className="mr-5 w-[40px] h-[35px]">
+                            <label htmlFor="img" className="hover:cursor-pointer">
+                                <UploadImg width={40} height={40} fill="#333333"/>
+                            </label>
+                                <input type="file" name="img" id="img" accept="image/png, image/jpeg" onChange={changeImg} className="w-[100%] h-[100%] bg-accent font-semibold hidden" />
+                        </div>
+                        <div className="mr-5 w-[100px] h-[35px]">
+                            <button className="w-[100%] h-[100%] bg-accent font-semibold" onClick={sendMessage}>Send</button>
+                        </div>
                     </div>
                 </div>
             </div>
