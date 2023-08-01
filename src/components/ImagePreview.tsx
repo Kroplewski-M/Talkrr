1af32a8da449714ff0948cd619c21e8f8648f49a
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2023-08-02 00:55:42 +0100

    changed img sizing

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -11,7 +11,7 @@ export const ImagePreview = ({ImgUrl,close}:ImagePreviewProps)=>{
         <div className="fixed top-0 left-0 z-[100] w-[100vw] h-[100%] backdrop-blur-md grid place-content-center">
             <div className="flex">
 
-                <img src={ImgUrl} alt="" className="w-[80%] h-[250px] md:w-[500px] md:h-[500px]"/>
+                <img src={ImgUrl} alt="" className="w-[200px] h-[200px] md:w-[500px] md:h-[500px]"/>
                 <div className="md:-mt-16 -mt-10 hover:cursor-pointer" onClick={close}>
                     <Close width={50} height={50} fill="#FF0000" />
                 </div>
